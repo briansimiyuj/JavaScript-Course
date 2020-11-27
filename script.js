63066a38abc7549e3266d40b814aed605f5e5e8c
briansimiyuj <briansimiyuj@gmail.com>
2020-11-27 09:51:39 +0300

    String slice

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-let myString =  'I am a champion'
+let str = 'hello, world'
 
-console.log(myString.indexOf('In'))
+let str1 = str.slice(2, 9)
+
+console.log(str1)
+
+let str2 = str.slice(2)
+
+console.log(str2)
